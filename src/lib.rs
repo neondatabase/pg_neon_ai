7401bcacc04aae2d4412b26c82ea386bf3118ace
George MacKerron <georgemackerron@neon.tech>
2024-07-05 12:26:29 +0100

    Added embedded bge_small_en_v15 model

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
+// use fastembed::{EmbeddingModel, InitOptions, TokenizerFiles}
+use fastembed::{UserDefinedEmbeddingModel, TextEmbedding, TokenizerFiles};
 use pgrx::prelude::*;
+
 pgrx::pg_module_magic!();
 
 extension_sql_file!("lib.sql");
 
-const ERR_PREFIX: &str = "[NEON_AI]"; 
+const ERR_PREFIX: &str = "[NEON_AI]";
 
 #[pg_extern]
 fn hello_neon_ai() -> &'static str {
     "Hello, neon_ai"
 }
-
 
 #[pg_extern]
 fn embedding_openai_raw(model: &str, input: &str, key: &str) -> pgrx::JsonB {
@@ -18,27 +20,64 @@ fn embedding_openai_raw(model: &str, input: &str, key: &str) -> pgrx::JsonB {
 
     match ureq::post("https://api.openai.com/v1/embeddings")
         .set("Authorization", auth.as_str())
-        .send_json(json_body) {
-            Err(ureq::Error::Transport(err)) => {
-                let msg = err.message().unwrap_or("no further details");
-                error!("{ERR_PREFIX} Transport error communicating with OpenAI API: {msg}");
-            },
-            Err(ureq::Error::Status(code, _)) => {  // unexpected status code (such as 400, 500 etc)
-                error!("{ERR_PREFIX} HTTP status code {code} trying to reach OpenAI API");
-            },
-            Ok(response) => {
-                match response.into_json() {
-                    Err(err) => {
-                        error!("{ERR_PREFIX} Failed to parse JSON received from OpenAI API: {err}", );
-                    },
-                    Ok(value) => {
-                        pgrx::JsonB(value)
-                    }
-                }
+        .send_json(json_body)
+    {
+        Err(ureq::Error::Transport(err)) => {
+            let msg = err.message().unwrap_or("no further details");
+            error!("{ERR_PREFIX} Transport error communicating with OpenAI API: {msg}");
+        }
+        Err(ureq::Error::Status(code, _)) => {
+            // unexpected status code (such as 400, 500 etc)
+            error!("{ERR_PREFIX} HTTP status code {code} trying to reach OpenAI API");
+        }
+        Ok(response) => match response.into_json() {
+            Err(err) => {
+                error!("{ERR_PREFIX} Failed to parse JSON received from OpenAI API: {err}");
             }
-        }    
+            Ok(value) => pgrx::JsonB(value),
+        },
+    }
 }
 
+#[pg_extern]
+fn embedding_bge_small_en_v15(input: Vec<&str>) -> Vec<Vec<f32>> {
+    let tokenizer_files = TokenizerFiles {
+        tokenizer_file: include_bytes!("../bge_small_en_v15/tokenizer.json").to_vec(),
+        config_file: include_bytes!("../bge_small_en_v15/config.json").to_vec(),
+        special_tokens_map_file: include_bytes!("../bge_small_en_v15/special_tokens_map.json")
+            .to_vec(),
+        tokenizer_config_file: include_bytes!("../bge_small_en_v15/tokenizer_config.json").to_vec(),
+    };
+
+    let user_def_model = UserDefinedEmbeddingModel {
+        onnx_file: include_bytes!("../bge_small_en_v15/model.onnx").to_vec(),
+        tokenizer_files: tokenizer_files,
+    };
+
+    // this downloads the model on-the-fly
+    // let model = match TextEmbedding::try_new(InitOptions {
+    //     model_name: EmbeddingModel::BGESmallENV15,
+    //     show_download_progress: false,
+    //     ..Default::default()
+    // }) {
+
+    let model = match TextEmbedding::try_new_from_user_defined(user_def_model, Default::default()) {
+        Err(err) => {
+            error!("{ERR_PREFIX} Couldn't load model bge_small_en_v15: {err}");
+        }
+        Ok(result) => result,
+    };
+
+    // generate embeddings with the default batch size, 256
+    let embeddings = match model.embed(input, None) {
+        Err(err) => {
+            error!("{ERR_PREFIX} Unable to generate bge_small_en_v15 embeddings: {err}",);
+        }
+        Ok(result) => result,
+    };
+
+    embeddings.clone()
+}
 
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
@@ -49,7 +88,6 @@ mod tests {
     fn test_hello_neon_ai() {
         assert_eq!("Hello, neon_ai", crate::hello_neon_ai());
     }
-
 }
 
 /// This module is required by `cargo pgrx test` invocations.
